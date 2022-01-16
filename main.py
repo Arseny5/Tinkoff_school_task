1c68cef5a051fd980e3445a5457f7a67d1ace172
Arseny <73182173+Arseny5@users.noreply.github.com>
2022-01-16 23:44:41 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,57 @@
+import pygame
+import time
+import classes
+
+FPS = 0
+pygame.init()
+sc = pygame.display.set_mode((1200, 800))
+pygame.display.set_caption("FPS = " + str(FPS))
+
+
+class desk_grafikal(classes.desk):
+    def __init__(self, w, h, a):
+        classes.desk.__init__(self, w, h, a)
+
+    def draw(self):
+        y = 0
+        for i in self.desk:
+            x = 0
+            for a in i:
+                if a == 1:
+                    pygame.draw.rect(sc, (0, 255, 0), (x * self.a, y * self.a, self.a, self.a))
+                else:
+                    pygame.draw.rect(sc, (255, 0, 0), (x * self.a, y * self.a, self.a, self.a))
+                x += 1
+            y += 1
+
+
+a = desk_grafikal(50, 50, 10)
+
+simulating = 0
+time0 = time.time()
+flRunning = True
+while flRunning:
+    time1 = time.time()
+    pygame.display.set_caption("FPS = " + str(FPS))
+
+    a.draw()
+    if simulating:
+        a.update()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            print("Закрытие окна...")
+            pygame.quit()
+            flRunning = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                a.kletka_klic(coord=event.pos)
+            elif event.button == 3:
+                if simulating:
+                    simulating = 0
+                else:
+                    simulating = 1
+    time0 = time1
+    pygame.display.flip()
+    #    time.sleep(0.05)
+    sc.fill((0, 0, 0))
